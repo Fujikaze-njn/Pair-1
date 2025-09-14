236a2008ac3364b030b5156c2d4b56ac9397df10
KING DAVID <xking6030@gmail.com>
2025-09-14 23:11:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -176,6 +176,5 @@ app.get("/pair", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Running on PORT:${port}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
