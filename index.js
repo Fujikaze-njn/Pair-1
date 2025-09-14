85e7c5429cf8a448b7df2b2aded43e8ffea92827
KING DAVID <xking6030@gmail.com>
2025-09-14 23:36:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const supabase = createClient(config.DBURL, config.SUPKEY);
 function generateSessionId() {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let randomPart = "";
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 16; i++) {
     randomPart += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return `Nexus_${randomPart}`;
